497510bdd4574e31a46f05585168f7d2adce97de
Mia-Y-Kelly <miayn24@gmail.com>
2024-07-10 15:27:10 -0500

    Update content; minor tweaks
    * Updated About and Experience
    * Adjusted spacing on About
    * Adjusted drop-shadow dark mode; fixed icon bug in dark mode

--- portfolio/pages/components/experience.jsx
+++ portfolio/pages/components/experience.jsx
@@ -18,11 +18,14 @@ export default function Experience() {
                         <h4>Nov. 2022 - Present</h4>
                     </div>
                     <ul>
-                        <li>Developing full-stack web application with interactive Linux terminal.</li>
-                        <li>Integrating OAuth Google authentication.</li>
-                        <li>Dockerizing application for minimal overhead.</li>
+                        <li>Led a group of six students in the development of an educational cybersecurity platform</li>
+                        <li>Created a web component that embedded an emulated terminal into the web browser using Xterm.js.</li>
+                        <li>Integrated OAuth Google authentication.</li>
+                        <li>Designed and implemented RESTful API using NodeJS and Express.</li>
+                        <li>Dockerized application for minimized overhead and platform agnostic development.</li>
+                        <li>Automated continuous production deployments using cron jobs on cPanel.</li>
                     </ul>
-                    <h4><b>Skills:</b> Express, Node, xterm, MySQL, Docker, OAuth, cPanel</h4>
+                    <h4><b>Skills:</b> Express, Node, xterm, MySQL, Docker, Google OAuth, cPanel, crontab, RESTful API</h4>
                 </div>
                 <div className={estyles.position}>
                     <h3>Full Stack Intern</h3>
@@ -32,11 +35,11 @@ export default function Experience() {
                     </div>
                     <ul>
                         <li>Tested a SpringBoot API which monitored activity between a mainframe and cloud environment.</li>
-                        <li>Performed Datadog analysis that reduced costs for log storage.</li>
+                        <li>Performed Datadog analysis that reduced costs by over $1,000 per month.</li>
                         <li>Renewed and retired container certificates using Jenkins and Kubernetes.</li>
                         <li>Consulted with subject matter experts to design a procedure for streamlining cloud application development.</li>
                     </ul>
-                    <h4><b>Skills:</b> SpringBoot, Datadog, Jenkins, Kubernetes, Agile (Kanban)</h4>
+                    <h4><b>Skills:</b> Java SpringBoot, Datadog, Jenkins, Kubernetes, Agile (Kanban)</h4>
                 </div>
                 <div className={estyles.position}>
                     <h3>IT Technician</h3>
@@ -47,9 +50,9 @@ export default function Experience() {
                     <ul>
                         <li>Replaced hardware on Dell, Lenovo, Mac, etc.</li>
                         <li>Performed troubleshooting for software issues on Windows, Mac, and RHEL operating systems.</li>
-                        <li>Provided on-site support for faculty with network, software, and hardware issues.</li>
+                        <li>Tracked client tickets using ServiceDesk and Confluence</li>
                     </ul>
-                    <h4><b>Skills:</b> Customer service, Windows, MacOS, RHEL</h4>
+                    <h4><b>Skills:</b> Customer service, ServiceDesk, Confluence, Windows, MacOS, RedHat Enterprise Linux</h4>
                 </div>
             </div>
         </>
